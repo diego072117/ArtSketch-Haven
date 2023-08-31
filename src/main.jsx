95c54c0dcd8d141fdcbd3aa1aadffed8c6c676f5
diego072117 <parracalderond9@gmail.com>
2023-08-31 18:25:43 -0500

    feat(routes-datafake): routes created and data fake.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ArtApp } from './ArtApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ArtApp />
   </React.StrictMode>,
 )
